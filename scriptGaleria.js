42b0e6559aab9f9010802c01b4cf6ec39b3adf33
ErikBaksay <79666150+ErikBaksay@users.noreply.github.com>
2021-10-12 16:59:31 +0200

    Contact Page Added

--- scriptGaleria.js
+++ scriptGaleria.js
@@ -1,35 +1,26 @@
 function goToDomov(){
     var Galeria = document.getElementById("contentGaleria");
-    var Title = document.getElementById("title");
     Galeria.classList.remove("slideIn");
-    Title.classList.remove("slideIn");
     Galeria.classList.add("slideAway");
-    Title.classList.add("slideAway");
     setTimeout(function(){
         window.location.href="index.html"
-    }, 500)
+    }, 400)
 }
 function goToGaleria(){
     var Galeria = document.getElementById("contentGaleria");
-    var Title = document.getElementById("title");
     Galeria.classList.remove("slideIn");
-    Title.classList.remove("slideIn");
     Galeria.classList.add("slideAway");
-    Title.classList.add("slideAway");
     setTimeout(function(){
         window.location.href="galeria.html"
-    }, 500)
+    }, 400)
 }
 function goToKontakt(){
     var Galeria = document.getElementById("contentGaleria");
-    var Title = document.getElementById("title");
     Galeria.classList.remove("slideIn");
-    Title.classList.remove("slideIn");
     Galeria.classList.add("slideAway");
-    Title.classList.add("slideAway");
     setTimeout(function(){
         window.location.href="kontakt.html"
-    }, 500)
+    }, 400)
 }
 function closeImage(){
     var Close = document.getElementById("close");
--- scriptKontakt.js
+++ scriptKontakt.js
@@ -1,33 +1,24 @@
 function goToDomov(){
     var Kontakt = document.getElementById("contentKontakt");
-    var Title = document.getElementById("title");
     Kontakt.classList.remove("slideIn");
-    Title.classList.remove("slideIn");
     Kontakt.classList.add("slideAway");
-    Title.classList.add("slideAway");
     setTimeout(function(){
         window.location.href="index.html"
-    }, 500)
+    }, 400)
 }
 function goToGaleria(){
     var Kontakt = document.getElementById("contentKontakt");
-    var Title = document.getElementById("title");
     Kontakt.classList.remove("slideIn");
-    Title.classList.remove("slideIn");
     Kontakt.classList.add("slideAway");
-    Title.classList.add("slideAway");
     setTimeout(function(){
         window.location.href="galeria.html"
-    }, 500)
+    }, 400)
 }
 function goToKontakt(){
     var Kontakt = document.getElementById("contentKontakt");
-    var Title = document.getElementById("title");
     Kontakt.classList.remove("slideIn");
-    Title.classList.remove("slideIn");
     Kontakt.classList.add("slideAway");
-    Title.classList.add("slideAway");
     setTimeout(function(){
         window.location.href="kontakt.html"
-    }, 500)
+    }, 400)
 }
